fcb0b222a09aa89a5832cc03aeb3736a8e2e69a5
Aravind <mymailidisapple@gmail.com>
2017-12-02 22:55:04 +0530

    Adds a module courses.component and added that in app.component.ts
    In this commit i have added some component ,added that as a dependency in NgModule in app module and then utilised that in app.component.html

--- AngularWebApp/src/app/app.module.ts
+++ AngularWebApp/src/app/app.module.ts
@@ -3,11 +3,12 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { CourseComponent } from './course.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,CourseComponent
   ],
   imports: [
     BrowserModule
