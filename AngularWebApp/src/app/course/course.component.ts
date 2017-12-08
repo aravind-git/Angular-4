5821d62b9a84605e9d4bde0ede512f059bcfef5a
Aravind <mymailidisapple@gmail.com>
2017-12-08 17:29:31 +0530

    Removes replicating file
    Removes a replicating file and made some changes to course.component.ts

--- AngularWebApp/src/app/course/course.component.ts
+++ AngularWebApp/src/app/course/course.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./course.component.css']
 })
 export class CourseComponent implements OnInit {
-title="just for fun";
+title="Aravind";
   constructor() { }
 
   ngOnInit() {
